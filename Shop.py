2eb537420ae3c35cc60efebfa56820163b5fd75a
Vladimir Fiodorov <81857359+Almaz-Py@users.noreply.github.com>
2021-06-24 15:05:44 +0300

    Update Shop.py

--- Shop.py
+++ Shop.py
@@ -1,16 +1,19 @@
 #Shop
 #id, addressId, workingHours (dict)
+from Adress import Address
 
 class Shop:
     def __init__(self, id, addressID, workingHours):
         self.id = id
         self.addressID = addressID
         self.workingHours = workingHours
+
     def __str__(self):
         return f"{self.id}, {self.addressID}, {self.workingHours}"
+
     def __repr__(self):
-        return f"{self.id}, {self.addressID}, {self.workingHours}"
+        return self.__str__()
 
 
-eShop = Shop(100, "www.100.md", {"Mo": "9.00-15.00", "Tu": "9.00-15.00", "We": "9.00-15.00", "Th": "9.00-15.00", "Fr": "9.00-15.00"  })
-print (eShop)
+
+
